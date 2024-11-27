50df859cae4c619f6b6ffb6037abb8e9b097a05f
Alfredo Diaz <patrik.17089@gmail.com>
2024-11-26 22:22:01 -0600

    Test

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import Services.Service;
 public class App {
     public static void main(String[] args) {
         Service service = new Service();
+        System.out.println("Hola mundo");
         service.menu();
     }
 }
